a071484e5d26970805598d0b921bceeeb31ec90d
Григорьева Марина <mgrigoreva@phoenixit.ru>
2022-08-30 22:03:45 +0300

    add logger

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,7 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
